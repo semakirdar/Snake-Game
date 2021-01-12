3c4587ee4a2955965ea0479461a34c74bd4eae89
sema <72384075+semakirdar@users.noreply.github.com>
2021-01-12 19:30:27 +0300

    java script snake game

---
+++ public/js/snakegame.js
@@ -0,0 +1,14 @@
+//const { default: MainScene } = require("./MainScene");
+
+
+import MainScene from './MainScene.js';
+
+const config = {
+  width: 640,
+  height: 640,
+  type: Phaser.AUTO,
+  parent: 'phaser-game',
+  scene: [MainScene]
+};
+
+new Phaser.Game(config);
